06582bf310edc271e4ffbc74adc8f3a74aa73817
Dori-Tos <dorian.smaelen@hotmail.com>
2025-10-01 13:51:06 +0200

    Add main & create file structure

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World! This is a DB Service for movies."
+
+@app.route("/actors")
+def actors():
+    return "List of actors will be here."
+
+@app.route("/reviews")
+def reviews():
+    return "List of reviews will be here."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
