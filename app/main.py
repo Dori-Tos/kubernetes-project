a6ed5757da0922c0654638d01e645c9817c3f8c4
ARTHURVANBELLE <23319@ecam.be>
2025-10-07 20:58:49 +0200

    testing fluxcd and new kustomization config

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! This is a DB Service for movies. FluxCD is working I think But you need to disable the cache in the browser to see the changes."
+    return "Hello, World! This is a DB Service for movies. I want to see this change made at 20:58 on 07/10/2025"
 
 @app.route("/actors")
 def actors():
