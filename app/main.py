0fbf20650dd3c8d7baeb09a02154bbc38ea86cf0
ARTHURVANBELLE <23319@ecam.be>
2025-10-08 15:29:43 +0200

    testing the automation workflow (flux + github actions)

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! This is a DB Service for movies. WE ARE ONLINE !! "
+    return "Hello, World! This is a DB Service for movies. WE ARE ONLINE !! I think "
 
 @app.route("/actors")
 def actors():
