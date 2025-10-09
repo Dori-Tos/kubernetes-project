45c19d161054cab4017e961bf6645aca69dc2e8e
ARTHURVANBELLE <23319@ecam.be>
2025-10-09 12:38:15 +0200

    testing fluxcd automatic deployment.yaml modification

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! This is a DB Service for movies. WE ARE ONLINE !! I think twice "
+    return "Hello, World! This is a DB Service for movies. WE ARE ONLINE !! I think twice 2 "
 
 @app.route("/actors")
 def actors():
