19b36576347f60c7a3cf154bd4a734af3c3bf3af
ARTHURVANBELLE <23319@ecam.be>
2025-10-13 11:58:41 +0200

    Using Nginx instead of Ingress

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! This is a DB Service for movies. COCORICOOO - 23:15 09-10-2023"
+    return "Hello, World! This is a DB Service for movies. Now accessible via movirating.local:30080"
 
 @app.route("/actors")
 def actors():
