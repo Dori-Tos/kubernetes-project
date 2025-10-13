965a44ddcfcb7fc8dbf55a5fa6ce474f002c678e
ARTHURVANBELLE <23319@ecam.be>
2025-10-13 16:12:03 +0200

    Test new CI workflow on github actions

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! This is a DB Service for movies. TEST"
+    return "Hello, World! This is a DB Service for movies. No changes here."
 
 @app.route("/actors")
 def actors():
